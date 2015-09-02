64c4cfcf063508b08eb0d4fa978897e35591cad7
Kevin <rojke34@gmail.com>
2015-09-02 12:33:07 -0500

    [cleaned] removed some spaces

--- LabFunction.js
+++ LabFunction.js
@@ -10,7 +10,20 @@ function triConvert(num){
   var unitMillion = '';
   var unitBillion = '';
   var output = '';
+  var msj = '';
   var numString = num.toString();
+
+  if (typeof num !== 'number') {
+    msj = { name: 'TypeError', message: 'Please, type in a number' };
+    
+    return msj.message;
+  }
+
+  if (numString.length > 12) {
+    msj = { name: 'TypeError', message: 'Warning, the number is the longest' };
+
+    return msj.message;
+  }
 
   // Zero case
   if (num === 0) {
@@ -20,6 +33,7 @@ function triConvert(num){
   //the case of 10, 11, 12 ,13, .... 19
   if (num < 20) {
     output = units[num];
+
     return output;
   }
 
@@ -28,6 +42,7 @@ function triConvert(num){
     output = units[parseInt(numString.charAt(0))] + hundred;
     output += tens[parseInt(numString.charAt(1))];
     output += units[parseInt(numString.charAt(2))];
+
     return output;
   }
 
@@ -37,32 +52,30 @@ function triConvert(num){
     output += units[parseInt(numString.charAt(1))] + hundred;
     output += tens[parseInt(numString.charAt(2))];
     output += units[parseInt(numString.charAt(3))];
+
     return output;
   }
 
   //10000 and more
   if (numString.length === 5) {
-
-    var deca = tens[parseInt(numString.charAt(0))];
-    deca += units[parseInt(numString.charAt(1))];
-    deca += thousand;
-
+    output = tens[parseInt(numString.charAt(0))];
+    output += units[parseInt(numString.charAt(1))] + thousand;
     output += units[parseInt(numString.charAt(2))] + hundred;
     output += tens[parseInt(numString.charAt(3))];
     output += units[parseInt(numString.charAt(4))];
-    return deca + output;
+
+    return output;
   }
 
   //100000 and more
   if (numString.length === 6) {
-
     output = units[parseInt(numString.charAt(0))] + hundred;
     output += tens[parseInt(numString.charAt(1))];
     output += units[parseInt(numString.charAt(2))] + thousand;
-
     output += units[parseInt(numString.charAt(3))] + hundred;
     output += tens[parseInt(numString.charAt(4))];
     output += units[parseInt(numString.charAt(5))];
+
     return output;
   }
 
@@ -73,10 +86,10 @@ function triConvert(num){
     output = units[parseInt(numString.charAt(1))] + hundred;
     output += tens[parseInt(numString.charAt(2))];
     output += units[parseInt(numString.charAt(3))] + thousand;
-
     output += units[parseInt(numString.charAt(4))] + hundred;
     output += tens[parseInt(numString.charAt(5))];
     output += units[parseInt(numString.charAt(6))];
+
     return unitMillion + output;
   }
 
@@ -89,10 +102,10 @@ function triConvert(num){
     output = units[parseInt(numString.charAt(2))] + hundred;
     output += tens[parseInt(numString.charAt(3))];
     output += units[parseInt(numString.charAt(4))] + thousand;
-
     output += units[parseInt(numString.charAt(5))] + hundred;
     output += tens[parseInt(numString.charAt(6))];
     output += units[parseInt(numString.charAt(7))];
+
     return unitMillion + output;
   }
 
@@ -105,10 +118,10 @@ function triConvert(num){
     output = units[parseInt(numString.charAt(3))] + hundred;
     output += tens[parseInt(numString.charAt(4))];
     output += units[parseInt(numString.charAt(5))] +  thousand;
-
     output += units[parseInt(numString.charAt(6))] + hundred;
     output += tens[parseInt(numString.charAt(7))];
     output += units[parseInt(numString.charAt(8))];
+
     return unitMillion + output;
   }
 
@@ -118,15 +131,15 @@ function triConvert(num){
 
     unitMillion = units[parseInt(numString.charAt(1))] + hundred;
     unitMillion += tens[parseInt(numString.charAt(2))];
-    unitMillion += units[parseInt(numString.charAt(3))] + milllon;
+    unitMillion += units[parseInt(numString.charAt(3))] + million;
 
     output = units[parseInt(numString.charAt(4))] + hundred;
     output += tens[parseInt(numString.charAt(5))];
     output += units[parseInt(numString.charAt(6))] + thousand;
-
     output += units[parseInt(numString.charAt(7))] + hundred;
     output += tens[parseInt(numString.charAt(8))];
     output += units[parseInt(numString.charAt(9))];
+
     return unitBillion + unitMillion + output;
   }
 
@@ -142,10 +155,10 @@ function triConvert(num){
     output = units[parseInt(numString.charAt(5))] + hundred;
     output += tens[parseInt(numString.charAt(6))];
     output += units[parseInt(numString.charAt(7))] + thousand;
-
     output += units[parseInt(numString.charAt(8))] + hundred;
     output += tens[parseInt(numString.charAt(9))];
     output += units[parseInt(numString.charAt(10))];
+
     return unitBillion + unitMillion + output;
   }
 
@@ -162,10 +175,10 @@ function triConvert(num){
     output = units[parseInt(numString.charAt(6))] + hundred;
     output += tens[parseInt(numString.charAt(7))];
     output += units[parseInt(numString.charAt(8))] + thousand;
-
     output += units[parseInt(numString.charAt(9))] + hundred;
     output += tens[parseInt(numString.charAt(10))];
     output += units[parseInt(numString.charAt(11))];
+
     return unitBillion + unitMillion + output;
   }
 
@@ -175,4 +188,4 @@ function triConvert(num){
   return output;
 }
 
-triConvert(878666384783);
+triConvert(8786663847832222222);
