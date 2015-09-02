7feb9da56b976a117e48bc896eaba08a65919fcc
Kevin <rojke34@gmail.com>
2015-09-02 12:25:00 -0500

    [refactoring] [cleaned]

--- LabFunction.js
+++ LabFunction.js
@@ -7,7 +7,8 @@ function triConvert(num){
   var thousand = ' thousand';
   var million = ' million';
   var billon = ' billon';
-
+  var unitMillion = '';
+  var unitBillion = '';
   var output = '';
   var numString = num.toString();
 
@@ -67,8 +68,7 @@ function triConvert(num){
 
   //1000000 and more
   if (numString.length === 7) {
-    var mill = '';
-    mill = units[parseInt(numString.charAt(0))] + million;
+    unitMillion = units[parseInt(numString.charAt(0))] + million;
 
     output = units[parseInt(numString.charAt(1))] + hundred;
     output += tens[parseInt(numString.charAt(2))];
@@ -77,14 +77,14 @@ function triConvert(num){
     output += units[parseInt(numString.charAt(4))] + hundred;
     output += tens[parseInt(numString.charAt(5))];
     output += units[parseInt(numString.charAt(6))];
-    return mill + output;
+    return unitMillion + output;
   }
 
   //10000000 and more
   if (numString.length === 8) {
-    var mill2 = tens[parseInt(numString.charAt(0))];
-    mill2 += units[parseInt(numString.charAt(1))];
-    mill2 += million;
+    unitMillion = tens[parseInt(numString.charAt(0))];
+    unitMillion += units[parseInt(numString.charAt(1))];
+    unitMillion += million;
 
     output = units[parseInt(numString.charAt(2))] + hundred;
     output += tens[parseInt(numString.charAt(3))];
@@ -93,15 +93,14 @@ function triConvert(num){
     output += units[parseInt(numString.charAt(5))] + hundred;
     output += tens[parseInt(numString.charAt(6))];
     output += units[parseInt(numString.charAt(7))];
-    return mill2 + output;
+    return unitMillion + output;
   }
 
   //100000000 and more
   if (numString.length === 9) {
-    var mill3 = units[parseInt(numString.charAt(0))] + hundred;
-    mill3 += tens[parseInt(numString.charAt(1))];
-    mill3 += units[parseInt(numString.charAt(2))];
-    mill3 += million;
+    unitMillion = units[parseInt(numString.charAt(0))] + hundred;
+    unitMillion += tens[parseInt(numString.charAt(1))];
+    unitMillion += units[parseInt(numString.charAt(2))] + million;
 
     output = units[parseInt(numString.charAt(3))] + hundred;
     output += tens[parseInt(numString.charAt(4))];
@@ -110,17 +109,16 @@ function triConvert(num){
     output += units[parseInt(numString.charAt(6))] + hundred;
     output += tens[parseInt(numString.charAt(7))];
     output += units[parseInt(numString.charAt(8))];
-    return mill3 + output;
+    return unitMillion + output;
   }
 
   //1000000000 and more
   if (numString.length === 10) {
-    var bill = units[parseInt(numString.charAt(0))] + billon;
+    unitBillion = units[parseInt(numString.charAt(0))] + billon;
 
-    var mill4 = units[parseInt(numString.charAt(1))] + hundred;
-    mill4 += tens[parseInt(numString.charAt(2))];
-    mill4 += units[parseInt(numString.charAt(3))];
-    mill4 += million;
+    unitMillion = units[parseInt(numString.charAt(1))] + hundred;
+    unitMillion += tens[parseInt(numString.charAt(2))];
+    unitMillion += units[parseInt(numString.charAt(3))] + milllon;
 
     output = units[parseInt(numString.charAt(4))] + hundred;
     output += tens[parseInt(numString.charAt(5))];
@@ -129,19 +127,17 @@ function triConvert(num){
     output += units[parseInt(numString.charAt(7))] + hundred;
     output += tens[parseInt(numString.charAt(8))];
     output += units[parseInt(numString.charAt(9))];
-    return bill + mill4 + output;
+    return unitBillion + unitMillion + output;
   }
 
   //10000000000 and more
   if (numString.length === 11) {
-    var bill1 = tens[parseInt(numString.charAt(0))];
-    bill1 += units[parseInt(numString.charAt(1))];
-    bill1 += billon;
+    unitBillion = tens[parseInt(numString.charAt(0))];
+    unitBillion += units[parseInt(numString.charAt(1))] + billon;
 
-    var mill5 = units[parseInt(numString.charAt(2))] + hundred;
-    mill5 += tens[parseInt(numString.charAt(3))];
-    mill5 += units[parseInt(numString.charAt(4))];
-    mill5 += million;
+    unitMillion = units[parseInt(numString.charAt(2))] + hundred;
+    unitMillion += tens[parseInt(numString.charAt(3))];
+    unitMillion += units[parseInt(numString.charAt(4))] + million;
 
     output = units[parseInt(numString.charAt(5))] + hundred;
     output += tens[parseInt(numString.charAt(6))];
@@ -150,20 +146,18 @@ function triConvert(num){
     output += units[parseInt(numString.charAt(8))] + hundred;
     output += tens[parseInt(numString.charAt(9))];
     output += units[parseInt(numString.charAt(10))];
-    return bill1 + mill5 + output;
+    return unitBillion + unitMillion + output;
   }
 
   //100000000000 and more
   if (numString.length === 12) {
-    var bill2 = units[parseInt(numString.charAt(0))] + hundred;
-    bill2 += tens[parseInt(numString.charAt(1))];
-    bill2 += units[parseInt(numString.charAt(2))];
-    bill2 += billon;
+    unitBillion = units[parseInt(numString.charAt(0))] + hundred;
+    unitBillion += tens[parseInt(numString.charAt(1))];
+    unitBillion += units[parseInt(numString.charAt(2))] + billon;
 
-    var mill6 = units[parseInt(numString.charAt(3))] + hundred;
-    mill6 += tens[parseInt(numString.charAt(4))];
-    mill6 += units[parseInt(numString.charAt(5))];
-    mill6 += million;
+    unitMillion = units[parseInt(numString.charAt(3))] + hundred;
+    unitMillion += tens[parseInt(numString.charAt(4))];
+    unitMillion += units[parseInt(numString.charAt(5))] + million;
 
     output = units[parseInt(numString.charAt(6))] + hundred;
     output += tens[parseInt(numString.charAt(7))];
@@ -172,7 +166,7 @@ function triConvert(num){
     output += units[parseInt(numString.charAt(9))] + hundred;
     output += tens[parseInt(numString.charAt(10))];
     output += units[parseInt(numString.charAt(11))];
-    return bill2 + mill6 + output;
+    return unitBillion + unitMillion + output;
   }
 
   output += tens[parseInt(numString.charAt(0))];
