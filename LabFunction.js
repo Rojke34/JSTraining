c58e84feffc9bc43fe16fd6390df45c9cf618ebc
Kevin <rojke34@gmail.com>
2015-09-02 12:20:24 -0500

    [refactoring] removed unnecessaty variables

--- LabFunction.js
+++ LabFunction.js
@@ -1,4 +1,4 @@
-clear();
+'use strict';
 
 function triConvert(num){
   var units = new Array('', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen');
@@ -86,9 +86,9 @@ function triConvert(num){
     mill2 += units[parseInt(numString.charAt(1))];
     mill2 += million;
 
-    deca3 = units[parseInt(numString.charAt(2))] + hundred;
-    deca3 += tens[parseInt(numString.charAt(3))];
-    deca3 += units[parseInt(numString.charAt(4))] + thousand;
+    output = units[parseInt(numString.charAt(2))] + hundred;
+    output += tens[parseInt(numString.charAt(3))];
+    output += units[parseInt(numString.charAt(4))] + thousand;
 
     output += units[parseInt(numString.charAt(5))] + hundred;
     output += tens[parseInt(numString.charAt(6))];
@@ -103,16 +103,14 @@ function triConvert(num){
     mill3 += units[parseInt(numString.charAt(2))];
     mill3 += million;
 
-    var deca4 = '';
-    deca4 = units[parseInt(numString.charAt(3))] + hundred;
-    deca4 += tens[parseInt(numString.charAt(4))];
-    deca4 += units[parseInt(numString.charAt(5))];
-    deca4 += thousand;
+    output = units[parseInt(numString.charAt(3))] + hundred;
+    output += tens[parseInt(numString.charAt(4))];
+    output += units[parseInt(numString.charAt(5))] +  thousand;
 
     output += units[parseInt(numString.charAt(6))] + hundred;
     output += tens[parseInt(numString.charAt(7))];
     output += units[parseInt(numString.charAt(8))];
-    return mill3 + deca4 + output;
+    return mill3 + output;
   }
 
   //1000000000 and more
@@ -124,16 +122,14 @@ function triConvert(num){
     mill4 += units[parseInt(numString.charAt(3))];
     mill4 += million;
 
-    var deca5 = '';
-    deca5 = units[parseInt(numString.charAt(4))] + hundred;
-    deca5 += tens[parseInt(numString.charAt(5))];
-    deca5 += units[parseInt(numString.charAt(6))];
-    deca5 += thousand;
+    output = units[parseInt(numString.charAt(4))] + hundred;
+    output += tens[parseInt(numString.charAt(5))];
+    output += units[parseInt(numString.charAt(6))] + thousand;
 
     output += units[parseInt(numString.charAt(7))] + hundred;
     output += tens[parseInt(numString.charAt(8))];
     output += units[parseInt(numString.charAt(9))];
-    return bill + mill4 + deca5 + output;
+    return bill + mill4 + output;
   }
 
   //10000000000 and more
@@ -147,16 +143,14 @@ function triConvert(num){
     mill5 += units[parseInt(numString.charAt(4))];
     mill5 += million;
 
-    var deca6 = '';
-    deca6 = units[parseInt(numString.charAt(5))] + hundred;
-    deca6 += tens[parseInt(numString.charAt(6))];
-    deca6 += units[parseInt(numString.charAt(7))];
-    deca6 += thousand;
+    output = units[parseInt(numString.charAt(5))] + hundred;
+    output += tens[parseInt(numString.charAt(6))];
+    output += units[parseInt(numString.charAt(7))] + thousand;
 
     output += units[parseInt(numString.charAt(8))] + hundred;
     output += tens[parseInt(numString.charAt(9))];
     output += units[parseInt(numString.charAt(10))];
-    return bill1 + mill5 + deca6 + output;
+    return bill1 + mill5 + output;
   }
 
   //100000000000 and more
@@ -171,16 +165,14 @@ function triConvert(num){
     mill6 += units[parseInt(numString.charAt(5))];
     mill6 += million;
 
-    var deca7 = '';
-    deca7 = units[parseInt(numString.charAt(6))] + hundred;
-    deca7 += tens[parseInt(numString.charAt(7))];
-    deca7 += units[parseInt(numString.charAt(8))];
-    deca7 += thousand;
+    output = units[parseInt(numString.charAt(6))] + hundred;
+    output += tens[parseInt(numString.charAt(7))];
+    output += units[parseInt(numString.charAt(8))] + thousand;
 
     output += units[parseInt(numString.charAt(9))] + hundred;
     output += tens[parseInt(numString.charAt(10))];
     output += units[parseInt(numString.charAt(11))];
-    return bill2 + mill6 + deca7 + output;
+    return bill2 + mill6 + output;
   }
 
   output += tens[parseInt(numString.charAt(0))];
